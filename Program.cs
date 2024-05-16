4b07f0b69e353bcde98c1e909ca743242d47c7bf
jhpenaor <58007769+jhpenaor@users.noreply.github.com>
2024-05-16 15:33:20 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Cambio 01
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
